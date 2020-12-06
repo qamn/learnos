c36ca2ddfbe8d4577e44afa44b1cbef0dad2615b
qamn <leoteyf90@163.com>
2020-12-06 17:17:52 +0800

    unf,alloc 1

--- kernel/memory/memory.c
+++ kernel/memory/memory.c
@@ -1,5 +1,47 @@
 #include "memory.h"
 
+unsigned long page_init(struct Page * page,unsigned long flags){
+    if(!page->attribute){
+        *(memory_management_struct.bits_map + ((page->PHY_address >> PAGE_2M_SHIFT) >> 6)) |= 1UL << (page->PHY_address >> PAGE_2M_SHIFT) % 64;
+        page->attribute = flags;
+        page->reference_count++;
+        page->zone_struct->page_using_count++;
+        page->zone_struct->page_free_count--;
+        page->zone_struct->total_pages_link++;
+    }else if((page->attribute & PG_Referenced) || (page->attribute & PG_K_Share_To_U) || (flags & PG_Referenced) || (flags & PG_K_Share_To_U)){
+        page->attribute |= flags;
+        page->reference_count++;
+        page->zone_struct->total_pages_link++;
+    }else{
+        *(memory_management_struct.bits_map + ((page->PHY_address >> PAGE_2M_SHIFT) >> 6)) |= 1UL << (page->PHY_address >> PAGE_2M_SHIFT) % 64;
+        page->attribute |= flags;
+    }
+    return 0;
+}
+
+unsigned long page_clean(struct Page * page){
+    if(!page->attribute){
+        page->attribute = 0;
+    }else if((page->attribute & PG_Referenced) || (page->attribute & PG_K_Share_To_U)){
+        page->reference_count--;
+        page->zone_struct->total_pages_link--;
+        if(!page->reference_count){
+            page->attribute = 0;
+            page->zone_struct->page_using_count--;
+            page->zone_struct->page_free_count++;
+        }
+    }else{
+        *(memory_management_struct.bits_map + ((page->PHY_address >> PAGE_2M_SHIFT) >> 6)) &= ~(1UL << (page->PHY_address >> PAGE_2M_SHIFT) % 64);
+
+        page->attribute = 0;
+        page->reference_count = 0;
+        page->zone_struct->page_using_count--;
+        page->zone_struct->page_free_count++;
+        page->zone_struct->total_pages_link--;
+    }
+    return 0;
+}
+
 void init_memory()
 {
     int i,j;
@@ -9,8 +51,7 @@ void init_memory()
 	// 读取内存信息, 只有类型1才是可用的
 	color_printk(BLUE,WHITE,"Display Physics Address MAP,Type(1:RAM,2:ROM or Reserved,3:ACPI Reclaim Memory,4:ACPI NVS Memory,Others:Undefine)\n");
 	p = (struct E820 *)0xffff800000007e00;
-	for(i = 0;i < 32;i++)
-	{
+	for(i = 0;i < 32;i++){
 		color_printk(ORANGE,BLACK,"Address:%#018lx\tLength:%#018lx\tType:%#010x\n",p->address,p->length,p->type);
 		unsigned long tmp = 0;
 		if(p->type == 1) //类型是1的就是可用的,才计算长度
@@ -25,9 +66,9 @@ void init_memory()
 		if(p->type > 4 || p->length == 0 || p->type < 1)
 			break;		
 	}
+    color_printk(ORANGE,BLACK,"OS Can Used Total RAM:%#018lx, e820length:%d\n",TotalMem,memory_management_struct.e820_length);
 
 	// 计算可用页的数量
-	color_printk(ORANGE,BLACK,"OS Can Used Total RAM:%#018lx\n",TotalMem);
 	TotalMem = 0;
 	for(i = 0;i <= memory_management_struct.e820_length;i++)
 	{
@@ -48,13 +89,11 @@ void init_memory()
     memory_management_struct.bits_size = TotalMem;
     memory_management_struct.bits_length = (((unsigned long)(memory_management_struct.bits_size) + sizeof(long) * 8 - 1) / 8) & ( ~ (sizeof(long) - 1));
     memset(memory_management_struct.bits_map,0xff,memory_management_struct.bits_length);		// 映射位图全部置1
-    color_printk(WHITE,BLACK,"bits_size: %d, bits_leangth: %d\n",memory_management_struct.bits_size,memory_management_struct.bits_length);
 
     memory_management_struct.pages_struct = (struct Page *)(((unsigned long)memory_management_struct.bits_map + memory_management_struct.bits_length + PAGE_4K_SIZE - 1) & PAGE_4K_MASK);
     memory_management_struct.pages_size = TotalMem;
     memory_management_struct.pages_length = (TotalMem * sizeof(struct Page) + sizeof(long) - 1) & ( ~ (sizeof(long) - 1));
     memset(memory_management_struct.pages_struct,0x00,memory_management_struct.pages_length);
-    color_printk(WHITE,BLACK,"pages_size: %d, pages_leangth: %d\n",memory_management_struct.pages_size,memory_management_struct.pages_length);
 
     memory_management_struct.zones_struct = (struct Zone *)(((unsigned long)memory_management_struct.pages_struct + memory_management_struct.pages_length + PAGE_4K_SIZE - 1) & PAGE_4K_MASK);
     memory_management_struct.zones_size   = 0; // 目前无法计算zone的个数, 置0
@@ -64,20 +103,20 @@ void init_memory()
     // 再次遍历E820来完成各数组成员变量的初始化
     for(i = 0;i <= memory_management_struct.e820_length;i++)
     {
+        if(memory_management_struct.e820[i].type != 1)
+            continue;
+
         unsigned long start,end;
         struct Zone * z;
         struct Page * p;
         unsigned long * b;
-
-        if(memory_management_struct.e820[i].type != 1)
-            continue;
         start = PAGE_2M_ALIGN(memory_management_struct.e820[i].address);
         end   = ((memory_management_struct.e820[i].address + memory_management_struct.e820[i].length) >> PAGE_2M_SHIFT) << PAGE_2M_SHIFT;
         if(end <= start)
             continue;
 
         //zone init
-        z = memory_management_struct.zones_struct + memory_management_struct.zones_size;
+        z = memory_management_struct.zones_struct + memory_management_struct.zones_size; //初始是0
         memory_management_struct.zones_size++;
         z->zone_start_address = start;
         z->zone_end_address = end;
@@ -110,4 +149,39 @@ void init_memory()
     memory_management_struct.pages_struct->reference_count = 0;
     memory_management_struct.pages_struct->age = 0;
 
+    memory_management_struct.zones_length = (memory_management_struct.zones_size * sizeof(struct Zone) + sizeof(long) - 1) & ( ~ (sizeof(long) - 1));
+    color_printk(ORANGE,BLACK,"bits_map:%#018lx,bits_size:%#018lx,bits_length:%#018lx\n",memory_management_struct.bits_map,memory_management_struct.bits_size,memory_management_struct.bits_length);
+    color_printk(ORANGE,BLACK,"pages_struct:%#018lx,pages_size:%#018lx,pages_length:%#018lx\n",memory_management_struct.pages_struct,memory_management_struct.pages_size,memory_management_struct.pages_length);
+    color_printk(ORANGE,BLACK,"zones_struct:%#018lx,zones_size:%#018lx,zones_length:%#018lx\n",memory_management_struct.zones_struct,memory_management_struct.zones_size,memory_management_struct.zones_length);
+
+    ZONE_DMA_INDEX = 0;	//need rewrite in the future
+    ZONE_NORMAL_INDEX = 0;	//need rewrite in the future
+
+    for(i = 0;i < memory_management_struct.zones_size;i++)	//need rewrite in the future
+    {
+        struct Zone * z = memory_management_struct.zones_struct + i;
+        color_printk(ORANGE,BLACK,"zone_start_address:%#018lx,zone_end_address:%#018lx,zone_length:%#018lx,pages_group:%#018lx,pages_length:%#018lx\n",z->zone_start_address,z->zone_end_address,z->zone_length,z->pages_group,z->pages_length);
+        if(z->zone_start_address == 0x100000000)
+            ZONE_UNMAPED_INDEX = i;
+    }
+
+    memory_management_struct.end_of_struct = (unsigned long)((unsigned long)memory_management_struct.zones_struct + memory_management_struct.zones_length + sizeof(long) * 32) & ( ~ (sizeof(long) - 1));	////need a blank to separate memory_management_struct
+    color_printk(ORANGE,BLACK,"start_code:%#018lx,end_code:%#018lx,end_data:%#018lx,end_brk:%#018lx,end_of_struct:%#018lx\n",memory_management_struct.start_code,memory_management_struct.end_code,memory_management_struct.end_data,memory_management_struct.end_brk, memory_management_struct.end_of_struct);
+
+    i = Virt_To_Phy(memory_management_struct.end_of_struct) >> PAGE_2M_SHIFT;
+
+    for(j = 0;j <= i;j++)
+    {
+        page_init(memory_management_struct.pages_struct + j,PG_PTable_Maped | PG_Kernel_Init | PG_Active | PG_Kernel);
+    }
+
+    Global_CR3 = Get_gdt();
+    color_printk(INDIGO,BLACK,"Global_CR3\t:%#018lx\n",Global_CR3);
+    color_printk(INDIGO,BLACK,"*Global_CR3\t:%#018lx\n",*Phy_To_Virt(Global_CR3) & (~0xff));
+    color_printk(PURPLE,BLACK,"**Global_CR3\t:%#018lx\n",*Phy_To_Virt(*Phy_To_Virt(Global_CR3) & (~0xff)) & (~0xff));
+
+    for(i = 0;i < 10;i++)
+        *(Phy_To_Virt(Global_CR3)  + i) = 0UL;
+
+    flush_tlb();
 }
